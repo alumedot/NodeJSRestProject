8f9422eebcfec9ffc0b1fdd169247e4b69e65175
Alexey Kozlov <alumedot@icloud.com>
2022-05-21 13:00:53 +0100

    feat: adjust get/post controllers

--- controllers/feed.ts
+++ controllers/feed.ts
@@ -4,8 +4,14 @@ export const getPosts: ExpressCB = (req, res) => {
   res.status(200).json({
     posts: [
       {
+        _id: '1',
         title: 'First Post',
-        content: 'Some content'
+        content: 'Some content',
+        imageUrl: 'images/dummy.png',
+        creator: {
+          name: 'Alex',
+        },
+        createdAt: new Date()
       }
     ]
   });
@@ -15,9 +21,11 @@ export const postPost: ExpressCB = ({ body: { title, content } }, res) => {
   res.status(201).json({
     message: 'Post created successfully!',
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title,
-      content
+      content,
+      creator: { name: 'Alex' },
+      createdAt: new Date()
     }
   });
 }
