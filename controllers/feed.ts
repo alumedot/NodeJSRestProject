21fbcf2878708ab9b3babe66218fa7218ee375da
Alexey Kozlov <alumedot@icloud.com>
2022-05-21 13:50:06 +0100

    feat: install express-validator, add validation to the feed post

--- controllers/feed.ts
+++ controllers/feed.ts
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import type { ExpressCB } from './types';
 
 export const getPosts: ExpressCB = (req, res) => {
@@ -17,7 +18,17 @@ export const getPosts: ExpressCB = (req, res) => {
   });
 }
 
-export const postPost: ExpressCB = ({ body: { title, content } }, res) => {
+export const postPost: ExpressCB = (req, res) => {
+  const { body: { title, content } } = req;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array()
+    })
+  }
+
   res.status(201).json({
     message: 'Post created successfully!',
     post: {
--- routes/feed.ts
+++ routes/feed.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { getPosts, postPost } from '../controllers/feed';
 
 export const router = express.Router();
 
 router.get('/posts', getPosts);
 
-router.post('/post', postPost);
+router.post(
+  '/post',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+  ],
+  postPost
+);
