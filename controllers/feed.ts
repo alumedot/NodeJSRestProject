bc016562e3e2ce45369b96885667e4fca7888b13
Alexey Kozlov <alumedot@icloud.com>
2022-05-30 22:39:23 +0100

    feat: edit posts

--- controllers/feed.ts
+++ controllers/feed.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { validationResult } from 'express-validator';
 import { Post } from '../models/post';
 import type { ExpressCB } from './types';
@@ -74,4 +76,60 @@ export const getPost: ExpressCB = async ({ params }, res, next) => {
     }
     next(e);
   }
+}
+
+export const updatePost: ExpressCB = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    (error as Error & { statusCode: number }).statusCode = 422;
+    throw error;
+  }
+
+  const { postId } = req.params;
+  const { title, content } = req.body;
+  let image = req.body.image;
+
+  if (req.file) {
+    image = req.file.path;
+  }
+
+  if (!image) {
+    const error = new Error('No image provided.');
+    (error as Error & { statusCode: number }).statusCode = 422;
+    throw error;
+  }
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error("Couldn't find post");
+      (error as Error & { statusCode: number }).statusCode = 404;
+      throw error;
+    }
+
+    if (image !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+
+    post.title = title;
+    post.imageUrl = image;
+    post.content = content;
+
+    const updatedPost = await post.save();
+
+    return res.status(200).json({ message: 'Post updated successfully', post: updatedPost });
+  } catch (e) {
+    if (!e.statusCode) {
+      e.statusCode = 500;
+    }
+    next(e);
+  }
+}
+
+const clearImage = (filePath) => {
+  const pathToDelete = path.join(__dirname, '..', filePath);
+  fs.unlink(pathToDelete, e => console.log(e));
 }
--- routes/feed.ts
+++ routes/feed.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { getPosts, postPost, getPost} from '../controllers/feed';
+import { getPosts, postPost, getPost, updatePost } from '../controllers/feed';
 
 export const router = express.Router();
 
@@ -9,10 +9,19 @@ router.get('/posts', getPosts);
 router.post(
   '/post',
   [
-    body('title').trim().isLength({ min: 7 }),
+    body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 })
   ],
   postPost
 );
 
 router.get('/post/:postId', getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+  ],
+  updatePost
+);
