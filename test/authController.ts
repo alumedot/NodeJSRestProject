009f6869b8072e4ddd6065e66f9adfa8cab54399
Alexey Kozlov <alumedot@icloud.com>
2022-07-30 17:28:30 +0100

    feat: using a test database

--- test/authController.ts
+++ test/authController.ts
@@ -1,10 +1,38 @@
 import { expect } from 'chai';
 import { stub } from 'sinon';
+import mongoose from 'mongoose';
 
 import { User } from '../models/user';
-import { login } from '../controllers/auth';
+import { login, getStatus } from '../controllers/auth';
 
-describe('Auth controller - login', () => {
+describe('Auth controller', () => {
+  before(function (done) {
+    mongoose.connect(process.env.TEST_MONGODB_URI)
+      .then(() => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '123412341234'
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      })
+  })
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  })
+
   it('Should throw an error with code 500 if accessing the database fails', (done) => {
     stub(User, 'findOne');
 
@@ -24,5 +52,29 @@ describe('Auth controller - login', () => {
     });
 
     (User.findOne as any).restore();
+  });
+
+  it('should send a response with a valid user status for an existing user', (done) => {
+      const request = {
+        userId: '123412341234',
+      };
+      const response = {
+        statusCode: 500,
+        userStatus: null,
+        status: function (code) {
+          this.statusCode = code;
+          return this;
+        },
+        json: function (data) {
+          this.userStatus = data.status;
+        }
+      };
+
+      (getStatus(request as any, response as any, () => {}) as any)
+        .then(() => {
+          expect(response.statusCode).to.be.equal(200);
+          expect(response.userStatus).to.be.equal('I am new');
+          done();
+        });
   });
 });
